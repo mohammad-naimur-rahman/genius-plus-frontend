2670b294212d6eddbed34a71fc26701c5df1e48c
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-07-16 22:51:01 +0600

    add: more strict eslint and prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,14 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+  singleQuote: true,
+  semi: false,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss']
+}
 
-export default config;
+export default config
