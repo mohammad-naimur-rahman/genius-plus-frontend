677411d4e2777424ffd00baf95fee87177d22e54
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-09-26 00:16:39 +0600

    add: image generation page header

--- src/app/[lang]/(rootLayout)/features/image/page.tsx
+++ src/app/[lang]/(rootLayout)/features/image/page.tsx
@@ -1,3 +1,8 @@
+import { ImagePlus } from 'lucide-react'
+import BackToFeatures from '~/components/reusable/common/BackToFeatures'
+import DashboardHeading from '~/components/reusable/dashboard/dashboard-heading'
+import { Button } from '~/components/ui/button'
+import Link from '~/components/ui/llink'
 import { genTitle } from '~/utils/misc/genTitle'
 
 export const metadata = {
@@ -5,5 +10,21 @@ export const metadata = {
 }
 
 export default function ImagePage() {
-  return <div>page</div>
+  return (
+    <div className='container'>
+      <DashboardHeading
+        title='AI Image Generator'
+        extra={
+          <>
+            <BackToFeatures />
+            <Link href='/features/image/generate'>
+              <Button icon={<ImagePlus />} iconPosition='right'>
+                Generate Image
+              </Button>
+            </Link>
+          </>
+        }
+      />
+    </div>
+  )
 }
